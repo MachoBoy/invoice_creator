bab7877081fa368ec7fd755bc5eedc698d79039c
machoboy <jiiwon0@gmail.com>
2016-10-25 22:13:17 -0700

    InvoiceCreated deleted, working on properties

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import update from 'react-addons-update'
 import { AppBar } from 'material-ui'
 import { TextField } from 'material-ui'
-import InvoiceCreate from './InvoiceCreate'
 import SideMenu from './SideMenu'
 
 
@@ -9,14 +9,53 @@ export default class Home extends React.Component {
     constructor(props){
         super(props);
         this.state = {
+            selectedKey: -1,
             open: false,
+            invoiceData: [{
+                invoiceNumber: '',
+                date: '',
+                companyName:'',
+                street: '',
+                city:'',
+                state:'',
+                postalCode:'',
+                phone:'',
+                billCompanyName:'',
+                billStreet: '',
+                billCity:'',
+                billState:'',
+                billPostalCode:'',
+                billPhone:'',
+                description: '',
+                amount: '',
+            }]
+
         };
+    }
+
+
+    handleClick(key) {
+    	this.setState({
+    		selectedKey: key
+    	});
+    	console.log(key, 'is selected');
     }
 
     toggleDrawer(){
         this.setState({
             open: !this.state.open
         });
+    }
+
+    handleEdit(data) {
+        this.setState({
+            invoiceData: update(this.state.invoiceData,
+            {
+                [this.state.selectedKey]: {
+                    invoiceNumber: {$set: data},
+                }
+            })
+        })
     }
 
     render() {
@@ -32,11 +71,12 @@ export default class Home extends React.Component {
                 <SideMenu
                     open={this.state.open}
                     onToggleDrawer={this.toggleDrawer.bind(this)}
+                    onCreate={this.handleEdit}
+
                 />
             <div>
-                <h1>Invoice</h1>
-                <TextField
-                />
+                <h1>start</h1>
+                <input>{this.props.invoiceNumber}</input>
             </div>
             </div>
 
